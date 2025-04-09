88a0850ce27babb99c8629e1fad79f0c2f559557
Saurav <saurav@Sauravs-MacBook-Air.local>
2025-04-09 16:55:33 +0530

    initial commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,8 +16,8 @@ export function Header() {
     <Container className="header-fixed">
       <Router>
         <HashLink smooth to="#home" className="logo">
-          <span>{"<Vinayak "}</span>
-          <span>{" Singh/>"}</span>
+          <span>{"<Saurav "}</span>
+          <span>{" Rai/>"}</span>
         </HashLink>
         <input
           onChange={toggleTheme}
